65345c191a12be364c59751f5a88d231a7464781
José <veganum@hotmail.com>
2020-08-27 12:30:55 +0200

    empezando top restaurant

--- app/screens/TopRestaurants.js
+++ app/screens/TopRestaurants.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { View, Text } from "react-native";
+import Toast from "react-native-easy-toast";
 
-export default function TopRestaurants() {
+import { firebaseApp } from "../utils/firebase";
+import firebase from "firebase";
+import "firebase/firestore";
+
+const db = firebase.firestore(firebaseApp);
+
+export default function TopRestaurants(props) {
+  const { navigation } = props;
+  const [restaurants, setRestaurants] = useState([]);
+  const toastRef = useRef();
+
+  console.log(restaurants);
+
+  useEffect(() => {
+    db.collection("restaurants")
+      .orderBy("rating", "desc")
+      .limit(5)
+      .get()
+      .then((response) => {
+        const restaurantArray = [];
+        response.forEach((doc) => {
+          const data = doc.data();
+          data.id = doc.id;
+          restaurantArray.push(data);
+        });
+        setRestaurants(restaurantArray);
+      });
+  }, []);
+
   return (
     <View>
       <Text>TopRestaurants..</Text>
+      <Toast ref={toastRef} position="center" opacity={0.9} />
     </View>
   );
 }
